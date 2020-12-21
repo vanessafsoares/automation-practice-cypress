9802c0e9c2adef2a4d5ea5b8ca9fb85b65c7f5b5
Vanessa Soares <vanessa.soares@PPM-SPO-8329.local>
2020-12-21 17:17:30 -0300

    adicionando cadastro e ajustes

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,10 +1,51 @@
-/// <reference types="cypress" />
-
-const faker = require('faker');
+const chance = require('chance').Chance()
 
 context('Cadastro', () => {
-    it('Efetuar cadastro de usuario', () => {
+  it('Efetuar cadastro de usuario', () => {
+    cy.visit('index.php?controller=authentication&back=my-account')
 
-        //implementar cenário aqui
-    });
-});
+    const email = chance.email()
+    const firstName = chance.first()
+    const lastName = chance.last()
+    const address = chance.address()
+    const city = chance.city()
+    const zipcode = chance.zip()
+    const mobile = chance.phone()
+
+    cy.get('input#email_create').type(email)
+
+    cy.get('button#SubmitCreate').click()
+
+    cy.get('input#id_gender2').check()
+
+    cy.get('input#customer_firstname').type(firstName)
+
+    cy.get('input#customer_lastname').type(lastName)
+
+    cy.get('input#passwd').type('TesteQA@123')
+
+    cy.get('select#days').select('15')
+
+    cy.get('select#months').select('November')
+
+    cy.get('select#years').select('1997')
+
+    cy.get('input#optin').check()
+
+    cy.get('input[name="address1"]').type(address)
+
+    cy.get('input[name="address2"]').type('Apartment 21')
+
+    cy.get('input[name="city"]').type(city)
+
+    cy.get('select#id_state').select('Kansas')
+
+    cy.get('input#postcode').type(zipcode)
+
+    cy.get('input#phone_mobile').type(mobile)
+
+    cy.get('button#submitAccount').click()
+
+    cy.url().should('contain', 'my-account')
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,8 +25,9 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('backgroundLogin', () => {
-    //background login 
-    cy.setCookie('PrestaShop-a30a9934ef476d11b6cc3c983616e364', 
-    'R6xmma6F4U6edNQuu67M0uiqqjjJHna5QCxjfxBx%2BO1EN8qvyuyk0v%2Bp3xWP%2Bx9sRfe2rtFrb2o8DG4L0qDJTWwOeXbK%2BNa%2BThdhVg5xJulrAyfGJF2bAxYHFGA8JZFRkAu%2BnyzS0caHx0Hwirfg%2Fi%2F29tdxQfZ1xip3xb%2BYfTM5OVWnPKDOreX5qrdIvmECTGXlFRUGTNBGE5yFNQ8PUwthUraqfP4tGAPjKFq%2BoASMMmkBqe45o6w83y286PveYlD7qYuDLHfckjYuXyGzTFfxNLXpC41CKn8kMnK2iywLxi%2BZi%2F%2BgTuWMDvwV6mB7uHmNaSDoKiKQga6QbVbdDrrYytmSBGmDNFZyWAwczQOC5sLSYJLKkbU%2BxlrUBdmMdRjMiXC7hDbrmzrVyshqo0Y1wSLPCgotLHlIpSvsW4E%3D000313')
-         
+  // background login
+  cy.setCookie(
+    'PrestaShop-a30a9934ef476d11b6cc3c983616e364',
+    'R6xmma6F4U6edNQuu67M0uiqqjjJHna5QCxjfxBx%2BO1EN8qvyuyk0v%2Bp3xWP%2Bx9sRfe2rtFrb2o8DG4L0qDJTWwOeXbK%2BNa%2BThdhVg5xJulrAyfGJF2bAxYHFGA8JZFRkAu%2BnyzS0caHx0Hwirfg%2Fi%2F29tdxQfZ1xip3xb%2BYfTM5OVWnPKDOreX5qrdIvmECTGXlFRUGTNBGE5yFNQ8PUwthUraqfP4tGAPjKFq%2BoASMMmkBqe45o6w83y286PveYlD7qYuDLHfckjYuXyGzTFfxNLXpC41CKn8kMnK2iywLxi%2BZi%2F%2BgTuWMDvwV6mB7uHmNaSDoKiKQga6QbVbdDrrYytmSBGmDNFZyWAwczQOC5sLSYJLKkbU%2BxlrUBdmMdRjMiXC7hDbrmzrVyshqo0Y1wSLPCgotLHlIpSvsW4E%3D000313',
+  )
 })
